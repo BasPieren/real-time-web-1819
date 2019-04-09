927f7af1704e51441b3f6d938e68af22f611bb57
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-04-09 13:31:21 +0200

    ✨ Show message on form submit with socket.io

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const express = require('express'),
-      io = require('socket.io'),
       app = express(),
+      http = require('http').Server(app),
+      io = require('socket.io')(http),
       port = 3000
 
 app
@@ -13,8 +14,24 @@ app
 
   .get('/', homePage)
 
-  .listen(process.env.PORT || port)
-
 function homePage(req, res) {
   res.render('pages/index.ejs')
 }
+
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    console.log('message ' + msg)
+    io.emit('chat message', msg)
+  })
+})
+
+http.listen(port, () => {
+  console.log('listening on *:3000');
+});
