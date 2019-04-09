c48a40755b1a7f4e3d915b2dc4a0b3bbc1a046d9
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-04-09 13:30:31 +0200

    ✨ Show message on form submit with socket.io

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello World!')
+'use strict';
+
+(() => {
+  const socket = io(),
+        form = document.querySelector('form'),
+        m = document.getElementById('m'),
+        ul = document.getElementById('messages')
+
+  form.onsubmit = (e => {
+    e.preventDefault()
+
+    socket.emit('chat message', m.value)
+
+    m.value = ''
+
+    return false
+  })
+  socket.on('chat message', msg => {
+    const li = document.createElement('li')
+
+    li.textContent = msg
+
+    ul.appendChild(li)
+  })
+})()
