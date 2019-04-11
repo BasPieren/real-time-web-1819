d44b03e940f30191faa079a05e463ad820e82bd5
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-04-11 09:39:56 +0200

    ✨ Created in-browser text editor and chatbox

--- public/js/script.js
+++ public/js/script.js
@@ -2,24 +2,69 @@
 
 (() => {
   const socket = io(),
-        form = document.querySelector('form'),
-        m = document.getElementById('m'),
-        ul = document.getElementById('messages')
+        textEditor = document.getElementById('rtw-text-editor-form'),
+        editorInput = document.getElementById('rtw-text-editor'),
+        chatBox = document.getElementById('rtw-chat-box-form'),
+        chatInput = document.getElementById('rtw-chat-box'),
+        chatMessages = document.getElementById('rtw-chat-messages')
 
-  form.onsubmit = (e => {
+  editorInput.focus()
+
+  textEditor.onsubmit = (e => {
     e.preventDefault()
 
-    socket.emit('chat message', m.value)
+    socket.emit('editor input', editorInput.value)
 
-    m.value = ''
+    editorInput.value = ''
+
+    return false
+  })
+  socket.on('editor input', input => {
+    const editorOutput = document.getElementById('rtw-text-editor-output'),
+          li = document.createElement('li')
+
+    if (input.startsWith('<') && input.endsWith('>')) {
+      const getAtrb = /(<)([\w\d]*)/g,
+            getAtrbVal = /(>)([\w\d\s\W]*)(<)/g
+
+      let atrb = getAtrb.exec(input),
+          atrbVal = getAtrbVal.exec(input),
+          createElement = document.createElement(atrb[2])
+
+      createElement.textContent = atrbVal[2]
+
+      editorOutput.appendChild(createElement)
+    } else if (input === '/help') {
+      const p = document.createElement('p')
+
+      p.textContent = 'Lets start with something simple, type "Hello World!" in a <h1> tag.'
+
+      editorOutput.appendChild(p)
+    } else {
+      const p = document.createElement('p')
+
+      p.textContent = 'Not a valid HTML tag'
+      p.className = 'rtw-not-valid'
+
+      editorOutput.appendChild(p)
+    }
+  })
+
+  chatBox.onsubmit = (e => {
+    e.preventDefault()
+
+    socket.emit('chat message', chatInput.value)
+
+    chatInput.value = ''
 
     return false
   })
   socket.on('chat message', msg => {
-    const li = document.createElement('li')
+    const chatOutput = document.getElementById('rtw-chat-messages'),
+          li = document.createElement('li')
 
     li.textContent = msg
 
-    ul.appendChild(li)
+    chatOutput.appendChild(li)
   })
 })()
