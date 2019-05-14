35fb8384d6f388e643d3f11e0342707e2af2de97
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-14 15:17:58 +0200

    🚧 Changed setInterval to 10000

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@ import * as repo_data from './modules/repo-data.js'
 function repoDataInterval() {
   repo_data.getRepoData()
 
-  setInterval(repo_data.getRepoData, 60000)
+  setInterval(repo_data.getRepoData, 10000)
 }
 
 getUserData()
