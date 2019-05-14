dccf7f01d88087b0dff7f3dec4a71c1e3e3621a6
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-14 10:22:38 +0200

    ✨ Save repo name to localStorage()

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,23 @@
 
 import { socket } from './modules/socket.js'
 import { getUserData } from './modules/user-data.js'
+import { getRepoData } from './modules/repo-data.js'
+import * as render from './modules/render-data.js'
 
-socket()
+function saveRepoName() {
+  const form = document.getElementById('rtw-create-repo')
+
+  if (document.body.contains(form)) {
+    form.addEventListener('submit', () => {
+      let repoName = document.getElementsByName('repo-name')[0].value
+
+      localStorage.setItem('repoName', repoName)
+    })
+  }
+}
+
+saveRepoName()
 getUserData()
+getRepoData()
+render.renderRepoData()
+// socket()
