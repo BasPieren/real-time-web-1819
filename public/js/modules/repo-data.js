b2ef6bf67c5b1084ecf8d9bb0c13e293675542b4
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-14 12:23:20 +0200

    ✨ Added saveRepoName() to repo-data.js

--- public/js/modules/repo-data.js
+++ public/js/modules/repo-data.js
@@ -1,3 +1,5 @@
+import * as render from './render-data.js'
+
 function getRepoData() {
   const getUrl = window.location.search.substring(1),
         userToken = getUrl.split('access_token=')[1],
@@ -15,10 +17,12 @@ function getRepoData() {
       .then(res => {
         console.log(res)
 
-        res.forEach( i => {
+        res.forEach(i => {
 
           if (i.name === repoName) {
             localStorage.setItem('repoData', JSON.stringify(i))
+
+            render.renderRepoData()
           }
         })
       })
@@ -26,4 +30,16 @@ function getRepoData() {
   }
 }
 
-export { getRepoData }
+function saveRepoName() {
+  const form = document.getElementById('rtw-create-repo')
+
+  if (document.body.contains(form)) {
+    form.addEventListener('submit', () => {
+      let repoName = document.getElementsByName('repo-name')[0].value
+
+      localStorage.setItem('repoName', repoName)
+    })
+  }
+}
+
+export { getRepoData, saveRepoName }
