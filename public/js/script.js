3649014ae8f6de62ed1fb6512e31cb0997497f7e
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-19 17:09:16 +0200

    ✨ Added seperate repoDataInterval() function

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,18 @@ import { socket } from './modules/socket.js'
 import { getUserData } from './modules/user-data.js'
 import * as repo_data from './modules/repo-data.js'
 
-// function repoDataInterval() {
-//   repo_data.getRepoData()
-//
-//   setInterval(repo_data.getRepoData, 10000)
-// }
+const form = document.getElementById('rtw-text-editor-form')
+
+function repoDataInterval() {
+  repo_data.getRepoData()
+
+  setInterval(repo_data.getRepoData, 10000)
+}
 
 getUserData()
 repo_data.saveRepoName()
-// repoDataInterval()
-socket()
+repoDataInterval()
+
+if (document.body.contains(form)) {
+  socket()
+}
