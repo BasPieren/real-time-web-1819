6f31eada96f3670c75fb894b2e6e39ca7a29f5de
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-19 17:10:43 +0200

    ♻️ Bunch of socket.js changes

--- public/js/modules/socket.js
+++ public/js/modules/socket.js
@@ -1,126 +1,112 @@
-import * as repo_data from './repo-data.js'
-
-function repoDataInterval() {
-  repo_data.getRepoData()
-
-  setInterval(repo_data.getRepoData, 10000)
-}
-
 function socket() {
-  const socket = io(),
-        textEditor = document.getElementById('rtw-text-editor-form'),
+  const textEditor = document.getElementById('rtw-text-editor-form'),
         editorInput = document.getElementById('rtw-text-editor'),
         editorOutput = document.getElementById('rtw-text-editor-output'),
         chatBox = document.getElementById('rtw-chat-box-form'),
         chatInput = document.getElementById('rtw-chat-box'),
         chatOutput = document.getElementById('rtw-chat-messages'),
         chatMessages = document.getElementById('rtw-chat-messages'),
-        form = document.getElementById('rtw-text-editor-form')
+        form = document.getElementById('rtw-text-editor-form'),
+        socket = io()
 
-  let userName = localStorage.getItem('userName')
+  let userName = localStorage.getItem('userName'),
+      getRepoName = localStorage.getItem('repoName'),
+      dashboard = io('/' + getRepoName)
 
-  if (document.body.contains(form)) {
+  editorInput.focus()
 
-    editorInput.focus()
+  // ---------- TEXT EDITOR ---------- //
 
-    // ---------- REPO DATA ---------- //
-    socket.on('repo data', () => {
-      repoDataInterval()
-    })
+  textEditor.onsubmit = (e => {
+    e.preventDefault()
 
-    // ---------- TEXT EDITOR ---------- //
+    dashboard.emit('editor input', editorInput.value)
 
-    textEditor.onsubmit = (e => {
-      e.preventDefault()
+    editorInput.value = ''
 
-      socket.emit('editor input', editorInput.value)
+    return false
+  })
+  dashboard.on('create element', (atrb, atrbVal) => {
+    let createElement = document.createElement(atrb[2])
 
-      editorInput.value = ''
+    createElement.textContent = atrbVal[2]
 
-      return false
-    })
-    socket.on('create element', (atrb, atrbVal) => {
-      let createElement = document.createElement(atrb[2])
+    editorOutput.appendChild(createElement)
+  })
+  dashboard.on('call command', command => {
+    const p = document.createElement('p')
 
-      createElement.textContent = atrbVal[2]
+    p.textContent = 'Lets start with something simple, type "Hello World!" in a <h1> tag.'
 
-      editorOutput.appendChild(createElement)
-    })
-    socket.on('call command', command => {
-      const p = document.createElement('p')
+    editorOutput.appendChild(p)
+  })
+  dashboard.on('editor input', input => {
+    const p = document.createElement('p')
 
-      p.textContent = 'Lets start with something simple, type "Hello World!" in a <h1> tag.'
+    p.textContent = 'Not a valid HTML tag'
+    p.className = 'rtw-not-valid'
 
-      editorOutput.appendChild(p)
-    })
-    socket.on('editor input', input => {
-      const p = document.createElement('p')
+    editorOutput.appendChild(p)
+  })
 
-      p.textContent = 'Not a valid HTML tag'
-      p.className = 'rtw-not-valid'
+  // ---------- CHATBOX ---------- //
 
-      editorOutput.appendChild(p)
-    })
+  chatBox.onsubmit = (e => {
+    e.preventDefault()
 
-    // ---------- CHATBOX ---------- //
+    dashboard.emit('chat message', chatInput.value)
 
-    chatBox.onsubmit = (e => {
-      e.preventDefault()
+    chatInput.value = ''
 
-      socket.emit('chat message', chatInput.value)
+    return false
+  })
+  dashboard.on('new message', splitMsg => {
+    let htmlElement = []
 
-      chatInput.value = ''
+    const htmlElementPush = splitMsg.map(e => {
+            if (e.startsWith('<') && e.endsWith('>')) {
+              const span = document.createElement('span')
 
-      return false
-    })
-    socket.on('new message', splitMsg => {
-      let htmlElement = []
+              span.textContent = e
+              span.className = 'rtw-chat-html'
 
-      const htmlElementPush = splitMsg.map(e => {
-              if (e.startsWith('<') && e.endsWith('>')) {
-                const span = document.createElement('span')
+              htmlElement.push(span)
+            }
+          }),
+          removeHTML = splitMsg.map(e => {
+            if (e.startsWith('<') === true) {
+              return ''
+            } else {
+              return e
+            }
+          }),
+          joinMsg = removeHTML.join(' ') ,
+          li = document.createElement('li')
 
-                span.textContent = e
-                span.className = 'rtw-chat-html'
+    li.textContent = userName + ': ' + joinMsg
 
-                htmlElement.push(span)
-              }
-            }),
-            removeHTML = splitMsg.map(e => {
-              if (e.startsWith('<') === true) {
-                return ''
-              } else {
-                return e
-              }
-            }),
-            joinMsg = removeHTML.join(' ') ,
-            li = document.createElement('li')
+    chatOutput.appendChild(li)
 
-      li.textContent = userName + ': ' + joinMsg
+    if (htmlElement.length > 0) {
+      li.appendChild(htmlElement[0])
+    }
+  })
+  dashboard.on('user connected', () => {
+    const li = document.createElement('li')
 
-      chatOutput.appendChild(li)
+    li.textContent = userName + ' connected'
+    li.className = 'rtw-user-connected'
 
-      if (htmlElement.length > 0) {
-        li.appendChild(htmlElement[0])
-      }
-    })
-    socket.on('user connected', () => {
-      const li = document.createElement('li')
+    chatOutput.appendChild(li)
+  })
+  dashboard.on('user disconnected', () => {
+    const li = document.createElement('li')
 
-      li.textContent = userName + ' connected'
-      li.className = 'rtw-user-connected'
+    li.textContent = userName + ' disconnected'
+    li.className = 'rtw-user-disconnected'
 
-      chatOutput.appendChild(li)
-    })
-    socket.on('user disconnected', () => {
-      const li = document.createElement('li')
-
-      li.textContent = userName + ' disconnected'
-      li.className = 'rtw-user-disconnected'
-
-      chatOutput.appendChild(li)
-    })
-  }
-  }
+    chatOutput.appendChild(li)
+  })
+}
 
 export { socket }
